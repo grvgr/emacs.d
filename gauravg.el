7b25b69c1aefc13d1ce99d65e8f4077eab8da1cb
Gaurav Giri <grv@grv.gr>
2010-12-08 18:28:05 +0530

    fullsceen hook

--- gauravg.el
+++ gauravg.el
@@ -90,6 +90,27 @@ point."
 (require 'maxframe)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
-
 ;; smooth scrolling
 (require 'smooth-scrolling)
+
+;; auto fill disable
+(auto-fill-mode -1)
+(set-default 'fill-column 200)
+
+
+;; fullscreen
+(defun toggle-fullscreen (&optional f)
+  (interactive)
+  (let ((current-value (frame-parameter nil 'fullscreen)))
+    (set-frame-parameter nil 'fullscreen
+                         (if (equal 'fullboth current-value)
+                             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                           (progn (setq old-fullscreen current-value)
+                                  'fullboth)))))
+
+(global-set-key [f11] 'toggle-fullscreen)
+;; Make new frames fullscreen by default. Note: this hook doesn't do
+;; anything to the initial frame if it's in your .emacs, since that file is
+;; read _after_ the initial frame is created.
+(add-hook 'after-make-frame-functions 'toggle-fullscreen)
+(global-set-key [f11] 'toggle-fullscreen)
