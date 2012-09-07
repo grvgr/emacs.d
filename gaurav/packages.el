eef3ef2478bf723ef5f9ba96f31251aacfa2e90e
Gaurav Giri <grv@grv.gr>
2012-09-07 22:25:40 +0530

    Adding packages.el to auto install packages on start. Removing starter kit files. They are still loaded from packages.

---
+++ gaurav/packages.el
@@ -0,0 +1,49 @@
+(setq grv-packages
+      '(
+        starter-kit-bindings
+        starter-kit-lisp
+        starter-kit-js
+        starter-kit-ruby
+        paredit
+        magit
+        smex
+        haml-mode
+        flymake-haml
+        js2-mode
+        ruby-mode
+        ruby-electric
+        rvm
+        sass-mode
+        flymake-sass
+        shellfm
+        wtf
+        yasnippet
+        yasnippet-bundle
+        auto-complete
+        autopair
+        coffee-mode
+        rinari
+        feature-mode
+        rainbow-mode
+        browse-kill-ring
+        color-theme
+        ))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(dolist (pkg grv-packages)
+  (when (and (not (package-installed-p pkg))
+             (assoc pkg package-archive-contents))
+    (package-install pkg)))
+
+(defun package-list-unaccounted-packages ()
+  "Like `package-list-packages', but shows only the packages that
+  are installed and are not in `grv-packages'.  Useful for
+  cleaning out unwanted packages."
+  (interactive)
+  (package-show-package-list
+   (remove-if-not (lambda (x) (and (not (memq x grv-packages))
+                              (not (package-built-in-p x))
+                              (package-installed-p x)))
+                  (mapcar 'car package-archive-contents))))
--- init.el
+++ init.el
@@ -6,4 +6,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("ELPA" . "http://tromey.com/elpa/"))
 (package-initialize)
