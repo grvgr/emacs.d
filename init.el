5c6afd6741da03550e5b9cf7f284b9bf8e1543ab
Gaurav Giri <grv@grv.gr>
2012-09-10 14:53:08 +0530

    Modifying init.el. Setting and loading custom.el. Adding emacs startup time to message log.

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+(defvar *emacs-load-start* (current-time))
+
+(setq esk-system-config (concat user-emacs-directory system-name ".el")
+      esk-user-config (concat user-emacs-directory user-login-name ".el")
+      esk-user-dir (concat user-emacs-directory user-login-name)
+      esk-custom-config (concat user-emacs-directory "custom.el"))
+
+(setq custom-file esk-custom-config)
+
 ;; Add Marmalade as package source
 (require 'package)
 (add-to-list 'package-archives
@@ -23,14 +32,18 @@
              (assoc pkg package-archive-contents))
     (package-install pkg)))
 
-;; moving starter kit autoload here
-(setq esk-system-config (concat user-emacs-directory system-name ".el")
-      esk-user-config (concat user-emacs-directory user-login-name ".el")
-      esk-user-dir (concat user-emacs-directory user-login-name))
+(load custom-file 'noerror)
 
+;; moving starter kit autoload here
 (add-to-list 'load-path esk-user-dir)
 (when (file-exists-p esk-system-config) (load esk-system-config))
 (when (file-exists-p esk-user-config) (load esk-user-config))
 (when (file-exists-p esk-user-dir) (mapc 'load (directory-files esk-user-dir nil "^[^#].*el$")))
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
+
+(message "Emacs load took %ds" (destructuring-bind (hi lo ms) (current-time)
+                                 (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
+;; Local Variables:
+;; no-byte-compile: t
+;; coding: utf-8
+;; End:
