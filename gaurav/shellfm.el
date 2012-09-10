96d51f726230c89064967db69e39a364a19e9ce3
Gaurav Giri <grv@grv.gr>
2012-09-10 11:48:35 +0530

    Shell.fm will now start if not already playing for M-p.

--- gaurav/shellfm.el
+++ gaurav/shellfm.el
@@ -2,15 +2,20 @@
 (require 'shellfm)
 
 (defun shellfm-notification ()
-  "Displays notification on status / track change"
+  "Displays notification on status / track change."
   (shell-command (format "/usr/bin/terminal-notifier -message \"%s - %s [%s]\" -title 'Shell.FM'"
-                          shellfm-current-artist
-                          shellfm-current-title
-                          shellfm-status))
+                         shellfm-current-artist
+                         shellfm-current-title
+                         shellfm-status))
   )
 
 (add-hook 'shellfm-status-hook 'shellfm-notification)
 (add-hook 'shellfm-track-hook  'shellfm-notification)
 
-(global-set-key "\M-p" 'shellfm-pause)
+(defun shellfm-start-or-toggle ()
+  "Starts shellfm if not running, otherwise toggles play / pause."
+  (interactive)
+  (if (equal shellfm-status 'dead) (shellfm 1) (shellfm-pause)))
+
+(global-set-key "\M-p" 'shellfm-start-or-toggle)
 (global-set-key "\M-n" 'shellfm-skip-track)
