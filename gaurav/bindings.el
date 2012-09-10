59e6f9fc3a856568bf3b00c93cccd0c7a891ec3b
Gaurav Giri <grv@grv.gr>
2012-09-10 14:52:45 +0530

    Adding more custom bindings.

--- gaurav/bindings.el
+++ gaurav/bindings.el
@@ -3,3 +3,9 @@
 (define-key input-decode-map "\e[1;5B" [C-down])
 (define-key input-decode-map "\e[1;5C" [C-right])
 (define-key input-decode-map "\e[1;5D" [C-left])
+
+;;; window control
+(global-set-key "\M-1" 'delete-other-windows)
+(global-set-key "\M-2" 'split-window-below)
+(global-set-key "\M-3" 'split-window-right)
+(global-set-key "\M-0" 'delete-window)
